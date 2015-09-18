b14d8e960fcc7e137db4637b1d7a1fb6b2843997
ajay <ajay@watchvast.com>
2015-09-18 16:32:20 -0400

    Added continuous loop, status messaging

--- speedtest/speedtest.py
+++ speedtest/speedtest.py
@@ -1,5 +1,6 @@
 from iobeam import iobeam
 
+import datetime
 import subprocess
 import time
 
@@ -13,22 +14,29 @@ PROJECT_TOKEN = # YOUR PROJECT TOKEN HERE (String)
 builder = iobeam.ClientBuilder(PROJECT_ID, PROJECT_TOKEN).saveToDisk().registerDevice()
 client = builder.build()
 
-# Get speedtest info
-# Note: You'll need to install speedtest-cli first (i.e., "pip install speedtest-cli")
-subprocess.call("speedtest-cli --bytes --simple > speedtest.out", shell=True)
+while True:
+  # Get speedtest info
+  # Note: You'll need to install speedtest-cli first (i.e., "pip install speedtest-cli")
+  subprocess.call("speedtest-cli --bytes --simple > speedtest.out", shell=True)
 
-download = float(subprocess.check_output("awk '/Download: /{print $2;}' speedtest.out", shell=True)[0:-1])
-upload = float(subprocess.check_output("awk '/Upload: /{print $2;}' speedtest.out", shell=True)[0:-1])
-ping = float(subprocess.check_output("awk '/Ping: /{print $2;}' speedtest.out", shell=True)[0:-1])
+  download = float(subprocess.check_output("awk '/Download: /{print $2;}' speedtest.out", shell=True)[0:-1])
+  upload = float(subprocess.check_output("awk '/Upload: /{print $2;}' speedtest.out", shell=True)[0:-1])
+  ping = float(subprocess.check_output("awk '/Ping: /{print $2;}' speedtest.out", shell=True)[0:-1])
 
-# Create new datapoints and send to iobeam
-now = int(time.time()*1000)
-d = iobeam.DataPoint(download, timestamp=now)
-u = iobeam.DataPoint(upload, timestamp=now)
-p = iobeam.DataPoint(ping, timestamp=now)
+  # Create new datapoints and send to iobeam
+  now = int(time.time()*1000)
+  d = iobeam.DataPoint(download, timestamp=now)
+  u = iobeam.DataPoint(upload, timestamp=now)
+  p = iobeam.DataPoint(ping, timestamp=now)
 
-# Look ma! I can send multiple data points at the same time!
-client.addDataPoint("download", d)
-client.addDataPoint("upload", u)
-client.addDataPoint("ping", p)
-client.send()
+  # Look ma! I can send multiple data points at the same time!
+  client.addDataPoint("download", d)
+  client.addDataPoint("upload", u)
+  client.addDataPoint("ping", p)
+  client.send()
+
+  dt = datetime.datetime.fromtimestamp(now/1000.0)
+  print 'Sent: (Download: %.2f Mbyte/s, Upload: %.2f Mbyte/s, Ping: %.3f ms) at %s' % (download, upload, ping, dt)
+
+  # Sleep for a bit (5 secs)
+  time.sleep(5)
